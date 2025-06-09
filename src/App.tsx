37790c7d9f3579c279e20d2122c14dc2031ed7c8
M-Lenvik <lenvik.marie@gmail.com>
2025-06-09 15:54:43 +0200

    fix: full screen view

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import { ToDoApp } from "./components/ToDoApp";
 
 function App() {
   return <>
-  <div className="max-w-screen-lg mx-auto p-4 bg-amber-400">
-
-    <ToDoApp/>
-  </div>
+    <div className="min-h-screen w-full bg-amber-400 p-4">
+      <div className="max-w-screen-lg mx-auto">
+        <ToDoApp/>
+      </div>
+    </div>
   </>;
 }
 
